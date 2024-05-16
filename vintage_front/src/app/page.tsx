557aecca8287e476bc0a5f45f0edd42f1993f7e5
Yash Bishnoi <112739888+nikhilDelu@users.noreply.github.com>
2024-05-16 12:05:48 +0530

    tested git

--- vintage_front/src/app/page.tsx
+++ vintage_front/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black text-white items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col bg-black text-white items-center justify-between p-24">
+      Home
+    </main>
   );
 }
