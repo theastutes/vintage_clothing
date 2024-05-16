2394b3e82e270faf27d9c967627a6cc9315bb539
Maurya-Karan <karanmaurya81002@gmail.com>
2024-05-16 12:01:13 +0530

    Changed favicon

--- vintage_front/src/app/page.tsx
+++ vintage_front/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black text-white items-center justify-between p-24">
-      Hello
+      
     </main>
   );
 }
