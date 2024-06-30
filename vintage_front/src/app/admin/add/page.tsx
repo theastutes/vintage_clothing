dc3fe62973187e86744d081241f21fde1bafc42a
Maurya-Karan <karanmaurya81002@gmail.com>
2024-06-30 21:29:10 +0530

    Fixed Add Product

--- vintage_front/src/app/admin/add/page.tsx
+++ vintage_front/src/app/admin/add/page.tsx
@@ -1,29 +1,15 @@
 "use client";
 import { z } from "zod";
-import {
-  Controller,
-  SubmitHandler,
-  useFieldArray,
-  useForm,
-} from "react-hook-form";
+import { Controller, SubmitHandler, useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import InputColor from "react-input-color";
+import InputColor from 'react-input-color'
 import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
-import { Input, Textarea } from "@nextui-org/input";
-import { Select, SelectSection, SelectItem } from "@nextui-org/select";
 import axios from "axios";
-<<<<<<< HEAD
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-const colorSchema = z.object({
-  color: z.string().min(1, "Color is required").default("#000000"),
-=======
 import { colors } from "@nextui-org/theme";
 
 const colorSchema = z.object({
   size:z.string(),
   color: z.string().min(1, "Color is required"),
->>>>>>> e9835d04915b4d796911550bf9c38e4c2095f319
   colorName: z.string().min(1, "Color name is required"),
   quantity: z.string().min(1, "Quantity is required"),
 });
@@ -45,7 +31,9 @@ const productSchema = z.object({
   colors:z.array(colorSchema),
 });
 
+
 type IProduct = z.infer<typeof productSchema>;
+
 
 const Form: React.FC = () => {
   const { register, control, getValues, setValue , watch , handleSubmit , formState: { errors , isSubmitting } } = useForm<IProduct>({ resolver: zodResolver(productSchema) });
@@ -93,9 +81,11 @@ const Form: React.FC = () => {
       console.log("Current Size : ",currentSizes,"/n sizeIndex", sizeIndex );
     }
 
+
+
   const showerors = () => {
     console.log(errors);
-  };
+  }
 
   function appendColor(size:string) {
     colAppend({size:size,color:'', colorName:'', quantity:''})
@@ -237,177 +227,12 @@ const Form: React.FC = () => {
             <button type="button" onClick={() => appendColor(sizefield.size) } >Add Color </ button>
             {errors.sizes && <p>{errors.sizes.message}</p>}
           </div>
+        ))}
+      </div>
 
-          {/* Details */}
-          <div className="relative w-full mb-4 flex flex-row gap-10 justify-center items-center">
-            <Label className="w-[10%]">Details</Label>
-            <Textarea
-              label="Description"
-              placeholder="Enter your Product description"
-              {...register("details")}
-              className="w-[90%]"
-            />
-            {errors.details && (
-              <p className="text-red-500">{errors.details.message}</p>
-            )}
-          </div>
-
-          {/* Images */}
-          <div className="relative w-full mb-4 flex flex-row gap-10 justify-center items-center">
-            <Label className="w-[10%]">Images</Label>
-            <Input
-              label="Images"
-              type="text"
-              {...register("images.0")}
-              className="w-[90%]"
-            />
-            {errors.images?.[0] && (
-              <p className="text-red-500">{errors.images[0].message}</p>
-            )}
-          </div>
-
-          {/* MRP */}
-          <div className="relative w-full mb-4 flex flex-row gap-10 justify-center items-center">
-            <Label className="w-[10%] truncate">MRP ₹</Label>
-            <Input
-              label="Mrp"
-              type="number"
-              {...register("mrp")}
-              className="w-[90%]"
-            />
-            {errors.mrp && <p className="text-red-500">{errors.mrp.message}</p>}
-          </div>
-
-          {/* SP */}
-          <div className="relative w-full mb-4 flex flex-row gap-10 justify-center items-center">
-            <Label className="w-[10%]">SP</Label>
-            <Input
-              label="Selling Price"
-              type="number"
-              {...register("sp")}
-              className="w-[90%]"
-            />
-            {errors.sp && <p className="text-red-500">{errors.sp.message}</p>}
-          </div>
-
-          {/* Category */}
-          <div className="relative w-full mb-4 flex flex-row gap-10 justify-center items-center">
-            <Label className="w-[10%]">Category</Label>
-            <Select
-              id="categories"
-              title="categories"
-              {...register("category")}
-              className="w-[90%]"
-            >
-              <SelectItem key="BTMLWR">Lower</SelectItem>
-              <SelectItem key="UPRSRT">Shirt</SelectItem>
-              <SelectItem key="UPRTSR"> Tshirt</SelectItem>
-              <SelectItem key="BTMJNS">Jeans</SelectItem>
-            </Select>
-            {errors.category && (
-              <p className="text-red-500">{errors.category.message}</p>
-            )}
-          </div>
-
-          {["S", "M", "L", "XL", "XXL", "XXXL"].map((size, index) => (
-            <div
-              className="w-full sm:w-1/2 rounded-sm border border-gray-500 p-2"
-              key={index}
-            >
-              <Checkbox
-                checked={fields.some((field) => field.size === size)}
-                onChange={(e) => toggleSizeSelection(size, e.target.checked)}
-              >
-                <Label>{size}</Label>
-              </Checkbox>
-            </div>
-          ))}
-
-          {/* Sizes */}
-          <div className="">
-            {fields.map((sizeField, index) => (
-              <div className=" " key={sizeField.id}>
-                <Label className="w-8 rounded-md bg-black/30 px-2">
-                  {sizeField.size}
-                </Label>
-                <div>
-                  {sizeField.colors.map((color, colorIndex) => (
-                    <div
-                      className="flex items-center justify-between gap-1"
-                      key={index}
-                    >
-                      <Controller
-                        control={control}
-                        name={
-                          `sizes.${index}.colors.${colorIndex}.color` as const
-                        }
-                        render={({ field }) => (
-                          <Input label="ColorName" {...field} />
-                        )}
-                      />
-                      {errors.sizes?.[index]?.colors?.[colorIndex]?.color && (
-                        <p>
-                          {
-                            errors.sizes?.[index]?.colors?.[colorIndex]?.color
-                              ?.message
-                          }
-                        </p>
-                      )}
-
-                      <Controller
-                        control={control}
-                        name={
-                          `sizes.${index}.colors.${colorIndex}.colorName` as const
-                        }
-                        render={({ field }) => (
-                          <InputColor
-                            initialValue={field.value} // Use the field's value as the initial color
-                            onChange={(color) => field.onChange(color.hex)} // Update form value when color changes
-                            placement="right"
-                          />
-                        )}
-                      />
-                      {errors.sizes?.[index]?.colors?.[colorIndex]
-                        ?.colorName && (
-                        <p>
-                          {
-                            errors.sizes?.[index]?.colors?.[colorIndex]
-                              ?.colorName?.message
-                          }
-                        </p>
-                      )}
-
-                      <Controller
-                        control={control}
-                        name={
-                          `sizes.${index}.colors.${colorIndex}.quantity` as const
-                        }
-                        render={({ field }) => (
-                          <Input label="Quantity" {...field} type="number" />
-                        )}
-                      />
-                      {errors.sizes?.[index]?.colors?.[colorIndex]
-                        ?.quantity && (
-                        <p>
-                          {
-                            errors.sizes?.[index]?.colors?.[colorIndex]
-                              ?.quantity?.message
-                          }
-                        </p>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                {errors.sizes && <p>{errors.sizes.message}</p>}
-              </div>
-            ))}
-          </div>
-
-          {/* <Button onClick={checksizes}>Check sizes</Button>
-          <Button name="show error" onClick={showerors}>
-            show errors
-          </Button> */}
-          {/* <button
+      <button type="button" onClick={checksizes}>Check sizes</button>
+      <button type="button" name="show error" onClick={showerors} >show errors</button>
+      {/* <button
         type="button"
         name="submit"
         onClick={handleSubmit(onSubmit)}
